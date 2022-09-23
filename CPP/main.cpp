b492da602aae8aa215894c26ed9fe413a41b0a1c
이경식 <leekyungsik@igyeongsig-ui-MacBookPro.local>
2022-09-23 18:54:53 +0900

    Initial cpp file

---
+++ CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPP
+//
+//  Created by 이경식 on 2022/09/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
