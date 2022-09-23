6046ce73339187357bc7e0f60e5aa3d96777cb2a
이경식 <leekyungsik@igyeongsig-ui-MacBookPro.local>
2022-09-23 19:01:52 +0900

    [TEST]

---
+++ CPP/1000 A+B.cpp
@@ -0,0 +1,15 @@
+//
+//  1000 A+B.cpp
+//  CPP
+//
+//  Created by 이경식 on 2022/09/23.
+//
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cin>>x>>y;
+    cout<<x+y<<endl;
+}
